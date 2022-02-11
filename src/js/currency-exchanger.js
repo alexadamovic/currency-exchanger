ba7b672c5165b6b99ab97568a8e539a25974918c
Alex Adamovic <alexanderadamovic@gmail.com>
2022-02-11 09:30:42 -0800

    change business logic to include necessary parameters in API endpoint

--- src/js/currency-exchanger.js
+++ src/js/currency-exchanger.js
@@ -1,6 +1,6 @@
 export default class CurrencyExchanger {
-  static exchangeRate() {
-    return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`)
+  static exchangeRate(userCurrency, userAmount) {
+    return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/USD/${userCurrency}/${userAmount}`)
       .then(function(response) {
         if (!response.ok) {
           throw Error(response.statusText);
