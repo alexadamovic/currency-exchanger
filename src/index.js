da554f5b799cdbf1ef6bcd3745ec20cc71f89cbe
Alex Adamovic <alexanderadamovic@gmail.com>
2022-02-11 10:00:34 -0800

    finish initial draft of UI logic

--- src/index.js
+++ src/index.js
@@ -13,16 +13,17 @@ function clearFields () {
   $('#show-error').hide();
 }
 
-// function getElements(response) {
-//   if (!response.bikes) {
-//     $('#show-errors').text(`Oopsie daisy! Something went wrong on our end. Error: ${response}`);
-//     return;
-//   } else if (response.bikes.length === 0) {
-//     $('#show-bikes').text(`No bikes have been reported stolen within 10 miles of this zip code.`);
-//   } else if (response.bikes) {
-//     $('#show-bikes').text(`The last bike that was stolen in your zipcode is a ${response.bikes[0].frame_colors.toString()} ${response.bikes[0].frame_model} ${response.bikes[0].manufacturer_name}`);
-//   }
-// }
+function getElements(response) {
+  if (response.result === "success") {
+    $('#user-good').show();
+    $('#currency-results').html(`${response.conversion_result} ${target_code}`);
+  } else if (response.result === "error") {
+    $('#show-bad').show();
+  } else {
+    $('show-error').show();
+    $('#non-user-error').html(`${response}`);
+  }
+}
 
 $(document).ready(function() {
   $('#bike-search').submit(function(event) {
